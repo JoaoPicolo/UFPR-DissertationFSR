319c81ae8cac8c69a7d330f348113d688fc35d68
João Pedro Picolo <picolo@Joaos-MacBook-Pro.local>
2023-03-20 08:48:26 -0300

    Improved code readability

--- src/didnet.py
+++ src/didnet.py
@@ -1,6 +1,6 @@
 from keras.layers import Layer, GlobalAveragePooling2D, Conv2D, ReLU, Activation, Multiply, Add, BatchNormalization
 from keras.models import Sequential
-
+from keras.utils import plot_model
 
 # CAL and RCAB are defined in: https://github.com/yulunzhang/RCAN
 class ChannelAttentionLayer(Layer):
@@ -12,21 +12,20 @@ class ChannelAttentionLayer(Layer):
 
     # TODO: Verify the valid of the padding value
     def build(self, input_shape):
-        self.avg_pool = GlobalAveragePooling2D(keepdims=True)
-        self.conv_du = Sequential(layers=[
+        self.body = Sequential(layers=[
+            GlobalAveragePooling2D(keepdims=True),
             Conv2D(filters=self.channel//self.reduction,
                    kernel_size=1, padding="same", use_bias=True),
             ReLU(),
             Conv2D(filters=self.channel, kernel_size=1,
                    padding="same", use_bias=True),
             Activation("sigmoid")
-        ])
+        ], name="CAL")
         super(ChannelAttentionLayer, self).build(input_shape)
 
     def call(self, input_data):
-        y = self.avg_pool(input_data)
-        y = self.conv_du(y)
-        return Multiply()([input_data, y])
+        res = self.body(input_data)
+        return Multiply()([res, input_data])
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.output_dim)
@@ -59,7 +58,7 @@ class ResidualChannelAttentionBlock(Layer):
         # TODO: Verify this one, it isn't in the original implementation
         modules_body.append(
             Conv2D(filters=3, kernel_size=self.kernel_size, padding="same", use_bias=True))
-        self.body = Sequential(layers=modules_body)
+        self.body = Sequential(layers=modules_body, name="RCAB")
         super(ResidualChannelAttentionBlock, self).build(input_shape)
 
     def call(self, input_data):
@@ -81,7 +80,7 @@ class BodyModule(Layer):
             modules.append(ResidualChannelAttentionBlock(
                 output_dim=self.output_dim))
 
-        self.body = Sequential(layers=modules)
+        self.body = Sequential(layers=modules, name="BM")
         super(BodyModule, self).build(input_shape)
 
     def call(self, input_data):
@@ -99,10 +98,12 @@ class UpsamplingBlock(Layer):
     def build(self, input_shape):
         # Add 1 body module
         # Add 1 upsampling module
-        # Add 1x1 con layer
+        # Add 1x1 conv layer
         self.body = Sequential(layers=[
-            BodyModule(output_dim=self.output_dim)
-        ])
+            BodyModule(output_dim=self.output_dim),
+            # TODO: Verifi if the number of filters is right
+            Conv2D(filters=64, kernel_size=1, activation='relu')
+        ], name="UB")
         super(UpsamplingBlock, self).build(input_shape)
 
     def call(self, input_data):
@@ -111,10 +112,10 @@ class UpsamplingBlock(Layer):
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.output_dim)
 
-
 def main():
-    g = Sequential()
+    g = Sequential(name="G_Model")
     g.add(UpsamplingBlock(32, input_shape=(128, 128, 3)))
+    print(g.summary(expand_nested=True))
 
 
 if __name__ == "__main__":
