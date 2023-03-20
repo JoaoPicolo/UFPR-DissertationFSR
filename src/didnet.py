fdfbe5d78301cb313e8ce8554dc883e272aa5028
João Pedro Picolo <picolo@Joaos-MacBook-Pro.local>
2023-03-20 11:19:42 -0300

    Refactoring

--- src/didnet.py
+++ src/didnet.py
@@ -1,12 +1,10 @@
 from keras.layers import Layer, GlobalAveragePooling2D, Conv2D, ReLU, Activation, Multiply, Add, BatchNormalization
-from keras.models import Sequential
-from keras.utils import plot_model
+from keras import Input, Model, Sequential
 
 # CAL and RCAB are defined in: https://github.com/yulunzhang/RCAN
 class ChannelAttentionLayer(Layer):
-    def __init__(self, output_dim, channel, reduction, **kwargs):
+    def __init__(self, channel, reduction, **kwargs):
         super(ChannelAttentionLayer, self).__init__(**kwargs)
-        self.output_dim = output_dim
         self.channel = channel
         self.reduction = reduction
 
@@ -27,16 +25,11 @@ class ChannelAttentionLayer(Layer):
         res = self.body(input_data)
         return Multiply()([res, input_data])
 
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.output_dim)
-
 
 class ResidualChannelAttentionBlock(Layer):
-    def __init__(self, output_dim,
-                 n_feat=64, kernel_size=3, reduction=16,
+    def __init__(self, n_feat=64, kernel_size=3, reduction=16,
                  bias=True, bn=False, res_scale=1, **kwargs):
         super(ResidualChannelAttentionBlock, self).__init__(**kwargs)
-        self.output_dim = output_dim
         self.n_feat = n_feat
         self.kernel_size = kernel_size
         self.reduction = reduction
@@ -53,8 +46,7 @@ class ResidualChannelAttentionBlock(Layer):
                 modules_body.append(BatchNormalization())
             if i == 0:
                 modules_body.append(ReLU())
-        modules_body.append(ChannelAttentionLayer(
-            output_dim=self.output_dim, channel=self.n_feat, reduction=self.reduction))
+        modules_body.append(ChannelAttentionLayer(channel=self.n_feat, reduction=self.reduction))
         # TODO: Verify this one, it isn't in the original implementation
         modules_body.append(
             Conv2D(filters=3, kernel_size=self.kernel_size, padding="same", use_bias=True))
@@ -65,57 +57,71 @@ class ResidualChannelAttentionBlock(Layer):
         res = self.body(input_data)
         return Add()([res, input_data])
 
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.output_dim)
-
 
 class BodyModule(Layer):
-    def __init__(self, output_dim, **kwargs):
-        self.output_dim = output_dim
+    def __init__(self, **kwargs):
         super(BodyModule, self).__init__(**kwargs)
 
     def build(self, input_shape):
         modules = []
         for _ in range(30):
-            modules.append(ResidualChannelAttentionBlock(
-                output_dim=self.output_dim))
+            modules.append(ResidualChannelAttentionBlock())
 
         self.body = Sequential(layers=modules, name="BM")
         super(BodyModule, self).build(input_shape)
 
     def call(self, input_data):
         return self.body(input_data)
+    
 
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.output_dim)
+class UpsamplingModule(Layer):
+    def __init__(self, **kwargs):
+        super(UpsamplingModule, self).__init__(**kwargs)
+
+    # Pixel-shuffle references this: https://keras.io/examples/vision/super_resolution_sub_pixel/#build-a-model
+    def build(self, input_shape):
+        self.body = Sequential(layers=[
+            # Pixel-shuffle
+            Conv2D(filters=64, kernel_size=5, activation='relu', padding="same", kernel_initializer="Orthogonal"),
+            Conv2D(filters=64, kernel_size=3, activation='relu', padding="same", kernel_initializer="Orthogonal"),
+            Conv2D(filters=32, kernel_size=3, activation='relu', padding="same", kernel_initializer="Orthogonal"),
+            Conv2D(filters=9,  kernel_size=3, activation='relu', padding="same", kernel_initializer="Orthogonal"),
+            # Conv 3x3
+            Conv2D(filters=3, kernel_size=3, activation='relu')
+        ], name="UM")
+        super(UpsamplingModule, self).build(input_shape)
+
+    def call(self, input_data):
+        return self.body(input_data)
 
 
 class UpsamplingBlock(Layer):
-    def __init__(self, output_dim, **kwargs):
-        self.output_dim = output_dim
+    def __init__(self, **kwargs):
         super(UpsamplingBlock, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        # Add 1 body module
-        # Add 1 upsampling module
-        # Add 1x1 conv layer
         self.body = Sequential(layers=[
-            BodyModule(output_dim=self.output_dim),
-            # TODO: Verifi if the number of filters is right
-            Conv2D(filters=64, kernel_size=1, activation='relu')
+            BodyModule(),
+            UpsamplingModule(),
+            # TODO: Verify if the number of filters is right
+            Conv2D(filters=3, kernel_size=1, activation='relu')
         ], name="UB")
         super(UpsamplingBlock, self).build(input_shape)
 
     def call(self, input_data):
         return self.body(input_data)
+    
 
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.output_dim)
+def getGModel():
+    inputs = Input(shape=(128,128,3))
+    outputs = UpsamplingBlock()(inputs)
+    # outputs = UpsamplingBlock()(outputs)
+    model = Model(inputs, outputs)
+    return model
 
 def main():
-    g = Sequential(name="G_Model")
-    g.add(UpsamplingBlock(32, input_shape=(128, 128, 3)))
-    print(g.summary(expand_nested=True))
+    g = getGModel()
+    print(g.summary())
 
 
 if __name__ == "__main__":
