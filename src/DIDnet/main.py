a1c5dddfba5d9abb738f93728c452f24305fd961
João Pedro Picolo <picolo@Joaos-MacBook-Pro.local>
2023-03-30 11:16:01 -0300

    Updated network loss

--- src/DIDnet/main.py
+++ src/DIDnet/main.py
@@ -10,7 +10,7 @@ from keras.callbacks import Callback, ModelCheckpoint
 from keras.utils import image_dataset_from_directory, array_to_img
 
 from generators import get_model_G, get_model_F
-from utils import charbonnier_loss, mse_from_embedding, bicubic_downsample
+from utils import charbonnier_loss, mae_loss, mse_from_embedding, bicubic_downsample
 
 sys.path.append("..")
 from Facenet.facenet import get_embeddings
@@ -22,25 +22,28 @@ class GANMonitor(Callback):
         self.num_img = num_img
 
     def on_epoch_end(self, epoch, logs=None):
-        _, ax = plt.subplots(4, 2, figsize=(12, 12))
-        for i, img in enumerate(self.test.take(self.num_img)):
-            prediction = self.model.gen_G(img)[0].numpy()
-            prediction = (prediction * 127.5 + 127.5).astype(np.uint8)
-            img = (img[0] * 127.5 + 127.5).numpy().astype(np.uint8)
+        epoch += 1
+        if (epoch % 10 == 0): # Saves every 10 epochs
+            _, ax = plt.subplots(4, 2, figsize=(12, 12))
+            for i, img in enumerate(self.test.take(self.num_img)):
+                prediction = self.model.gen_G(img)
 
-            ax[i, 0].imshow(img)
-            ax[i, 1].imshow(prediction)
-            ax[i, 0].set_title("Input image")
-            ax[i, 1].set_title("Translated image")
-            ax[i, 0].axis("off")
-            ax[i, 1].axis("off")
+                img = np.array(img.numpy())
+                img = img[0, :, :, :]
+                img = array_to_img(img)
 
-            prediction = array_to_img(prediction)
-            # prediction.save(
-            #     "generated_img_{i}_{epoch}.png".format(i=i, epoch=epoch + 1)
-            # )
-        plt.show()
-        plt.close()
+                prediction = np.array(prediction.numpy())
+                prediction = prediction[0, :, :, :]
+                prediction = array_to_img(prediction)
+
+                ax[i, 0].imshow(img)
+                ax[i, 1].imshow(prediction)
+                ax[i, 0].set_title("Input image")
+                ax[i, 1].set_title("Translated image")
+                ax[i, 0].axis("off")
+                ax[i, 1].axis("off")
+
+            plt.savefig(f"./results/results_{epoch}.png")
 
 class DIDnet(Model):
     def __init__(
@@ -54,12 +57,14 @@ class DIDnet(Model):
 
     def compile(
         self,
+        optimizer,
         gen_G_optimizer,
         gen_F_optimizer,
         cycle_loss_fn,
         identity_loss_fn
     ):
         super().compile()
+        self.optimizer = optimizer
         self.gen_G_optimizer = gen_G_optimizer
         self.gen_F_optimizer = gen_F_optimizer
         self.cycle_loss_fn = cycle_loss_fn
@@ -85,6 +90,7 @@ class DIDnet(Model):
             cycle_loss_F = self.cycle_loss_fn(real_y, cycled_y)
 
             # TODO: Verify if this is not inverted
+            # Get face embeddings from Facenet
             id_embeddings_G = get_embeddings(fake_x, cycled_x)
             id_embeddings_F = get_embeddings(fake_y, cycled_y)
 
@@ -98,21 +104,32 @@ class DIDnet(Model):
             total_loss_G = cycle_loss_G + id_loss_G
             total_loss_F = cycle_loss_F + id_loss_F
 
-        # Get the gradients for the generators
-        grads_G = tape.gradient(total_loss_G, self.gen_G.trainable_variables)
-        grads_F = tape.gradient(total_loss_F, self.gen_F.trainable_variables)
+            # Network Loss
+            loop_loss = total_loss_G + total_loss_F
+            identity_loss = id_loss_G + id_loss_F
+            rec_loss = mae_loss(real_y, fake_y)
+            channel_loss = charbonnier_loss(tf.image.rgb_to_yuv(fake_y), tf.image.rgb_to_yuv(real_y))
+            network_loss = loop_loss + rec_loss + 0.5*identity_loss + 0.5*channel_loss
 
-        # Update the weights of the generators
+        # Gets the gradients for the generators and optimizes them
+        grads_G = tape.gradient(total_loss_G, self.gen_G.trainable_variables)
         self.gen_G_optimizer.apply_gradients(
             zip(grads_G, self.gen_G.trainable_variables)
         )
+
+        grads_F = tape.gradient(total_loss_F, self.gen_F.trainable_variables)
         self.gen_F_optimizer.apply_gradients(
             zip(grads_F, self.gen_F.trainable_variables)
         )
 
+        # Gets the gradients for the whole network and optimizes it
+        grads_network = tape.gradient(network_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads_network, self.trainable_variables))
+
         return {
             "G_loss": total_loss_G,
             "F_loss": total_loss_F,
+            "DIDnet_loss": network_loss,
         }
 
 
@@ -125,6 +142,7 @@ def main():
 
     # Compile the model
     didnet.compile(
+        optimizer=Adam(learning_rate=2e-4, beta_1=0.5),
         gen_G_optimizer=Adam(learning_rate=2e-4, beta_1=0.5),
         gen_F_optimizer=Adam(learning_rate=2e-4, beta_1=0.5),
         cycle_loss_fn=charbonnier_loss,
@@ -132,9 +150,9 @@ def main():
     )
 
     train = image_dataset_from_directory(
-        directory="../../datasets/FEI/", validation_split=0.025, subset="training", seed=123, image_size=(360, 260), batch_size=None)
+        directory="../../datasets/FEI_test/", validation_split=0.1, subset="training", seed=123, image_size=(360, 260), batch_size=None)
     test = image_dataset_from_directory(
-        directory="../../datasets/FEI/", validation_split=0.025, subset="validation", seed=123, image_size=(360, 260), batch_size=None)
+        directory="../../datasets/FEI_test/", validation_split=0.1, subset="validation", seed=123, image_size=(360, 260), batch_size=None)
 
     train_sr = (train.map(lambda x, _: x).batch(1))
     test_sr = (test.map(lambda x, _: x).batch(1))
@@ -143,14 +161,14 @@ def main():
 
     # Callbacks
     plotter = GANMonitor(test=test_lr)
-    checkpoint_filepath = ".didnet_checkpoints.{epoch:03d}"
+    checkpoint_filepath = "./checkpoints/didnet_checkpoints.{epoch:03d}"
     model_checkpoint_callback = ModelCheckpoint(
-        filepath=checkpoint_filepath
+        filepath=checkpoint_filepath, save_weights_only=True
     )
 
     didnet.fit(
         tf.data.Dataset.zip((train_lr, train_sr)),
-        epochs=1,
+        epochs=5,
         callbacks=[plotter, model_checkpoint_callback],
     )
 
--- src/DIDnet/utils.py
+++ src/DIDnet/utils.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 
 def charbonnier_loss(y_true, y_pred):
-    return tf.reduce_mean(tf.sqrt(tf.square(y_true - y_pred) + tf.square(1e-6)))
+    return tf.reduce_mean(tf.sqrt(tf.square(y_true - y_pred) + 1e-6))
+
+
+def mae_loss(y_true, y_pred):
+    return tf.reduce_mean(tf.abs(y_true - y_pred))
 
 
 def mse_from_embedding(fst_embedding, snd_embedding):
@@ -9,8 +13,9 @@ def mse_from_embedding(fst_embedding, snd_embedding):
     for idx, _ in enumerate(fst_embedding):
         sum += (fst_embedding[idx] - snd_embedding[idx])**2
 
-    # TODO: Verify if in the original paper it takes the mean
+    sum = sum / len(fst_embedding)
     return sum
+
 
 def bicubic_downsample(img, _):
     bicubic = tf.image.resize(
